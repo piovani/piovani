bc7a0143e523520f43915cb89db37f8febf29ed3
Allison Piovani <apiovani25@gmail.com>
2019-06-03 22:15:58 -0300

    Iniciando o projeto e conf Vuefity.

---
+++ src/components/base/footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1 class="">Ola eu sou o footer</h1>
+</template>
+
+<script>
+    export default {
+        name: "footer"
+    }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import './plugins/vuetify'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -8,6 +9,7 @@ import './registerServiceWorker'
 Vue.config.productionTip = false
 
 new Vue({
+  iconfont: 'md',
   router,
   store,
   render: h => h(App)
