be76de23f939a109b668b49adcea92a61aaafcdc
Allison Piovani <apiovani25@gmail.com>
2019-06-11 21:34:39 -0300

    iniciando o desenvolvimento do footer

--- src/components/base/footer.vue
+++ src/components/base/footer.vue
@@ -1,13 +1,63 @@
 <template>
-    <h1 class="">Ola eu sou o footer</h1>
+    <div id="footer">
+        <div id="conteudo">&copy; {{ getAno() }} - Allison Piovani</div>
+        <div id="redes-sociais">
+            <a class="btn" v-for="(rede, index) in redes" :key="index" :src="rede.link">
+                {{ rede.name }}
+            </a>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "footer"
+        name: "footer",
+
+        data: () => ({
+            redes: [
+                {
+                    name: 'facebook',
+                    icone: 'fab fa-facebook',
+                    link: ''
+                },
+                {
+                    name: 'github',
+                    icone: 'fab fa-facebook',
+                    link: ''
+                },
+                {
+                    name: 'linkedin',
+                    icone: 'fab fa-linkedin',
+                    link: ''
+                },
+                {
+                    name: 'twitter',
+                    icone: 'fab fa-twitter',
+                    link: ''
+                }
+            ]
+        }),
+
+        methods: {
+            getAno() {
+                let now = new Date;
+
+                return now.getFullYear();
+            }
+        }
     }
 </script>
 
-<style lang="sass" scoped>
+<style scoped>
+    #footer {
+        border: black 1px solid;
+    }
 
+    #conteudo {
+        border: red 1px solid;
+    }
+
+    #redes-sociais {
+        border: blue 1px solid;
+    }
 </style>
