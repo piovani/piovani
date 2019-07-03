f36aa7ef6bcba8832c1e1da0e90d6a241a267967
Allison Piovani <apiovani25@gmail.com>
2019-07-02 23:26:14 -0300

    Add icons in project

--- src/components/base/footer.vue
+++ src/components/base/footer.vue
@@ -1,12 +1,30 @@
 <template>
-    <div id="footer">
-        <div id="conteudo">&copy; {{ getAno() }} - Allison Piovani</div>
-        <div id="redes-sociais">
-            <a class="btn" v-for="(rede, index) in redes" :key="index" :src="rede.link">
-                {{ rede.name }}
-            </a>
-        </div>
-    </div>
+    <v-footer dark height="auto">
+        <v-card class="flex" tile>
+            <v-card-title class="primary">
+                <strong class="subheading">&copy;{{getAno()}} – Allison Piovani</strong>
+
+                <v-spacer></v-spacer>
+
+                <v-btn class="mx-4" dark icon>
+                    <img src="@/assets/facebook.png" />
+                </v-btn>
+
+                <v-btn v-for="rede in redes" class="mx-4 secondary" dark icon>
+                    <img :src="rede.icone">
+<!--                    <v-icon size="24px">{{ rede.icone }}</v-icon>-->
+                </v-btn>
+            </v-card-title>
+        </v-card>
+    </v-footer>
+    <!--    <div id="footer">-->
+    <!--        <div id="conteudo">&copy; {{ getAno() }} - Allison Piovani</div>-->
+    <!--        <div id="redes-sociais">-->
+    <!--            <a class="btn" v-for="(rede, index) in redes" :key="index" :src="rede.link">-->
+    <!--                {{ rede.name }}-->
+    <!--            </a>-->
+    <!--        </div>-->
+    <!--    </div>-->
 </template>
 
 <script>
@@ -17,24 +35,31 @@
             redes: [
                 {
                     name: 'facebook',
-                    icone: 'fab fa-facebook',
-                    link: ''
-                },
-                {
-                    name: 'github',
-                    icone: 'fab fa-facebook',
-                    link: ''
-                },
-                {
-                    name: 'linkedin',
-                    icone: 'fab fa-linkedin',
-                    link: ''
-                },
-                {
-                    name: 'twitter',
-                    icone: 'fab fa-twitter',
+                    icone: '@/assets/facebook.png',
                     link: ''
                 }
+                // {
+                //     name: 'github',
+                //     icone: 'fab fa-facebook',
+                //     link: ''
+                // },
+                // {
+                //     name: 'linkedin',
+                //     icone: 'fab fa-linkedin',
+                //     link: ''
+                // },
+                // {
+                //     name: 'twitter',
+                //     icone: 'fab fa-twitter',
+                //     link: ''
+                // }
+            ],
+            icons: [
+                'fab fa-facebook',
+                'fab fa-twitter',
+                'fab fa-google-plus',
+                'fab fa-linkedin',
+                'fab fa-instagram'
             ]
         }),
 
